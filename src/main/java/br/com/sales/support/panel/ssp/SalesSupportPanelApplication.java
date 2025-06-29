94ef86d132ac55612e5d7b669313a3a69a99cd8c
Leonardo Marques <leomarques301@gmail.com>
2025-06-29 15:56:57 -0300

    chore: Initial setup

---
+++ src/main/java/br/com/sales/support/panel/ssp/SalesSupportPanelApplication.java
@@ -0,0 +1,13 @@
+package br.com.sales.support.panel.ssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSupportPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSupportPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sales/support/panel/ssp/SalesSupportPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sales.support.panel.ssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSupportPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
