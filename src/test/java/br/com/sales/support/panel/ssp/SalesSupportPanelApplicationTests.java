dd6fb2e59cd4f002384021fe7e4da275798210d3
Leonardo Marques <leomarques301@gmail.com>
2025-07-02 20:49:24 -0300

    chore: Add Testcontainers support for integration testing
    Introduced Testcontainers dependencies and configuration for PostgreSQL in test scope. Updated SalesSupportPanelApplicationTests to use a PostgreSQL container for integration tests. Moved InMemoryCampaignRepository to the test source directory.

--- src/test/java/br/com/sales/support/panel/ssp/SalesSupportPanelApplicationTests.java
+++ src/test/java/br/com/sales/support/panel/ssp/SalesSupportPanelApplicationTests.java
@@ -2,9 +2,25 @@ package br.com.sales.support.panel.ssp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SalesSupportPanelApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
